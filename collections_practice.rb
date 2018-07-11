76d13352bbbc8f87cac132ea968a951c0f47d537
Andrew Och <andrewoch95@gmail.com>
2018-07-11 17:17:18 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -40,6 +40,6 @@ end
 
 def count_elements (array)
   
-array.each_with_object(Hash.new(0)) { |word,counts| counts[word] += 1 }
+array.each_with_object(Hash.new(0)) { |word,counts| {count: counts[word] += 1 }
   
 end
