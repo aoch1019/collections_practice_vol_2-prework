98d221a19317c56c9f1c13d0c74f60a5addd1b3d
Andrew Och <andrewoch95@gmail.com>
2018-07-11 16:57:51 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -44,6 +44,6 @@ def count_elements (array)
     h[g[:name]] += 1
   end
   
-  map { |name, count| { :name=>name, :count=>count } }
+  array.map { |name, count| { :name=>name, :count=>count } }
   
 end
